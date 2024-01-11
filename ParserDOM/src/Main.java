47efe95532b214523247c34ab4160d8ed7eb10f9
Alberto Naranjo <zenycs3@gmail.com>
2024-01-11 21:15:30 +0100

    Parser XML DOM

---
+++ ParserDOM/src/Main.java
@@ -0,0 +1,79 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        parser();
+    }
+    public static void parser() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setIgnoringComments(true);
+        dbf.setIgnoringElementContentWhitespace(true);
+
+        try {
+            System.out.println("Parser DOM");
+            File inputFile = new File("prueba.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            muestraNodo(doc);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    public static void muestraNodo(Node nodo) {
+        switch (nodo.getNodeType()) {
+            case Node.DOCUMENT_NODE:
+                System.out.println("Nodo Documento");
+                muestraNodoDocumento(nodo);
+                break;
+            case Node.ELEMENT_NODE:
+                System.out.println("Nodo Documento");
+                muestraNodoElemento(nodo);
+                break;
+            case Node.TEXT_NODE:
+                if(nodo.getNodeValue().trim().length() == 0) {
+                    return;
+                }
+                System.out.println("Nodo texto");
+                muestraNodoTexto(nodo);
+                break;
+            default:
+                System.out.println("Otro tipo de nodo");
+        }
+        NodeList hijos = nodo.getChildNodes();
+        System.out.println("****************************");
+        for (int i=0; i < hijos.getLength(); i++) {
+            muestraNodo(hijos.item(i));
+        }
+    }
+    public static void muestraNodoDocumento(Node nodo) {
+
+    }
+
+    public static void muestraNodoElemento(Node nodo) {
+        System.out.println(nodo.getNodeName());
+        NamedNodeMap NNN = nodo.getAttributes();
+        if (NNN.getLength() > 0) {
+            for (int i = 0; i < NNN.getLength();i++) {
+                System.out.println(NNN.item(i));
+            }
+        }
+    }
+
+    public static void muestraNodoTexto(Node nodo) {
+
+    }
+}
