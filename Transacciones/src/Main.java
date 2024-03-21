e8b833aff449ab026ee41cf72c82da6168035bba
Alberto Naranjo <zenycs3@gmail.com>
2024-03-21 20:04:24 +0100

    ingresoQuery + retiradaQuery + transferencia

--- Transacciones/src/Main.java
+++ Transacciones/src/Main.java
@@ -35,17 +35,21 @@ public class Main {
                     break;
                 // Retirada de dinero
                 case 2:
+                    retirada(conex);
                     break;
                 // Transferencia entre cuentas
                 case 3:
+                    transferencia(conex);
                     break;
                 // Balance de cuenta
                 case 4:
                     break;
+                // Listar tabla cuentas
+                case 5:
+                    break;
             }
         }
     }
-
     public static void ingreso(String[] conex) throws SQLException {
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Cual es el número de cuenta?");
@@ -54,13 +58,75 @@ public class Main {
         float ingreso = sc.nextFloat();
 
         // Comprobamos si existe el numero de cuenta
-        checkCuenta(conex, cuentaOrigen);
+       if(checkCuenta(conex, cuentaOrigen)) {
+           ingresoQuery(conex,ingreso,cuentaOrigen);
+       } else {
+           System.out.println("El número de cuenta no existe");
+       }
+    }
 
+    public static void retirada(String[] conex) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cual es el número de cuenta?");
+        String cuentaOrigen = sc.nextLine();
+        System.out.println("¿Qué cantidad desea retirar?");
+        float cantidad = sc.nextFloat();
+
+        // Comprobamos si existe el numero de cuenta
+        if(checkCuenta(conex, cuentaOrigen)) {
+            if(checkSaldo(conex,cuentaOrigen,cantidad)) {
+                retiradaQuery(conex,cantidad,cuentaOrigen);
+            } else {
+                System.out.println("Estás intentando retirar más dinero del disponible");
+            }
+        } else {
+            System.out.println("El número de cuenta no existe");
+        }
+    }
+
+    public static void transferencia(String[] conex) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cual es la cuenta de origen?");
+        String cuentaOrigen = sc.nextLine();
+        System.out.println("¿Cual es la cuenta de destino?");
+        String cuentaDestino = sc.nextLine();
+        System.out.println("¿Qué cantidad desea transferir?");
+        float cantidad = sc.nextFloat();
+        if(checkCuenta(conex, cuentaDestino)) {
+            if (checkSaldo(conex,cuentaOrigen,cantidad)) {
+                retiradaQuery(conex,cantidad,cuentaOrigen);
+                ingresoQuery(conex, cantidad, cuentaDestino);
+            } else {
+                System.out.println("Estás intentando retirar más dinero del disponible");
+            }
+        } else {
+            System.out.println("La cuenta no existe");
+        }
+    }
+    public static boolean checkCuenta(String[] conex, String cuenta) throws SQLException {
+        Connection c = DriverManager.getConnection(conex[0], conex[1], conex[2]);
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("SELECT * FROM bdbanco.cuentas WHERE NumCuenta='" + cuenta + "'");
+        return rs.isBeforeFirst();
+    }
+    public static boolean checkSaldo(String[] conex, String cuenta, float cantidad) throws SQLException {
+        Connection c = DriverManager.getConnection(conex[0], conex[1], conex[2]);
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("SELECT SUM(Saldo  + -" + cantidad + ") AS result " +
+                "FROM cuentas WHERE NumCuenta= " + cuenta + " HAVING result > 0");
+        if (!rs.isBeforeFirst()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void ingresoQuery(String[] conex, float ingreso, String cuentaOrigen) throws SQLException {
         Connection c = DriverManager.getConnection(conex[0], conex[1], conex[2]);
         try (Statement s = c.createStatement()) {
             c.setAutoCommit(false);
-            s.execute("SELECT SUM(Saldo + " + "ingreso" + ") FROM cuentas" +
-                    " WHERE CuentaOrigen='" + cuentaOrigen + "'");
+            s.execute("UPDATE cuentas " +
+                    "SET Saldo=(SELECT SUM(Saldo + " + ingreso + ") WHERE NumCuenta='" + cuentaOrigen + "')" +
+                    " WHERE NumCuenta='" + cuentaOrigen + "'");
             c.commit();
         } catch (SQLException e) {
             try {
@@ -74,13 +140,23 @@ public class Main {
         }
     }
 
-    public static boolean checkCuenta(String[] conex, String cuenta) throws SQLException {
+    public static void retiradaQuery(String[] conex, float cantidad, String cuentaOrigen) throws SQLException {
         Connection c = DriverManager.getConnection(conex[0], conex[1], conex[2]);
-        Statement s = c.createStatement();
-        ResultSet rs = s.executeQuery("SELECT * FROM bdbanco.cuentas WHERE CuentaOrigen='" + cuenta + "'");
-        if (!rs.isBeforeFirst()) {
-            return false;
+        try (Statement s = c.createStatement()) {
+            c.setAutoCommit(false);
+            s.execute("UPDATE cuentas SET Saldo=(SELECT SUM(Saldo + -" + cantidad + ") " +
+                    "WHERE NumCuenta='" + cuentaOrigen + "') " +
+                    "WHERE NumCuenta='" + cuentaOrigen + "'");
+            c.commit();
+        } catch (SQLException e) {
+            try {
+                c.rollback();
+                System.err.println("Se ha realizado un rollback");
+                System.out.println("Excepcion: " + e.getMessage());
+            } catch (SQLException ex) {
+                System.err.println("Se ha producido un error al hacer el rollback");
+                System.out.println("Excepcion: " + ex.getMessage());
+            }
         }
-        return true;
     }
 }
