a7cf749fb56ff49580618211a68cd328b8ebadb9
AlbertoCodex <zenycs3@gmail.com>
2024-09-30 20:36:31 +0200

    Menu + Metodos de Clase RandomAccessFile

--- RandomAccessFile/src/FicheroAccesoAleatorio.java
+++ RandomAccessFile/src/FicheroAccesoAleatorio.java
@@ -2,45 +2,50 @@ import java.io.*;
 
 public class FicheroAccesoAleatorio {
     RandomAccessFile raf;
-    FileReader fr;
-    FileWriter fw;
-    BufferedReader br;
-    BufferedWriter bw;
 
     // Constructor
     public FicheroAccesoAleatorio(String nombre) throws IOException {
-        fr = new FileReader(nombre);
-        fw = new FileWriter(nombre);
-        br = new BufferedReader(fr);
-        bw = new BufferedWriter(fw);
         raf = new RandomAccessFile(nombre,"rw");
     }
     // Escribe el texto en el punto donde se encuentre el cursor del archivo.
-    public void escribir(String texto) {
-
+    public void escribir(String texto) throws IOException {
+        raf.writeBytes(" " + texto + " ");
     }
     // Coloca el cursor en la posición indicada.
-    public void ir(long posicion) {
-
+    public void ir(long posicion) throws IOException {
+        raf.seek(posicion);
     }
     // Adelanta el cursor en las posiciones indicadas.
-    public void adelante(long posiciones) {
-
+    public void adelante(long posiciones) throws IOException {
+        long posActual = raf.getFilePointer();
+        raf.seek(posActual + posiciones);
     }
     // Atrasa el cursor en las posiciones indicadas.
-    public void atras(long posiciones) {
-
+    public void atras(long posiciones) throws IOException {
+        long posActual = raf.getFilePointer();
+        raf.seek(posActual - posiciones);
     }
     // Muestra por pantalla el caracter apuntado por el cursor.
-    public void leer() {
-
+    public void leer() throws IOException {
+        // readChar() da EOF Exception
+        int value = raf.read();
+        char letra = (char) value;
+        System.out.println(letra);
     }
     // Muestra el resto de la línea de la linea donde se encuentra el cursor.
-    public void leerLinea() {
-
+    public void leerLinea() throws IOException {
+        String line = raf.readLine();
+        System.out.println(line);
     }
     // Muestra el contenido de un fichero.
-    public void mostrarFichero() {
+    public void mostrarFichero() throws IOException {
+        String line;
+        while ((line = raf.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
 
+    public RandomAccessFile getRaf() {
+        return raf;
     }
 }
--- RandomAccessFile/src/Main.java
+++ RandomAccessFile/src/Main.java
@@ -1,8 +1,67 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
         // Ruta = Argumento 0
         FicheroAccesoAleatorio faa = new FicheroAccesoAleatorio(args[0]);
+        menu(faa);
+    }
+
+    public static void menu(FicheroAccesoAleatorio faa) throws IOException {
+        int x = -1;
+
+        while (x != 0) {
+            System.out.println("El puntero está en la posición: " + faa.getRaf().getFilePointer());
+            menuPreguntas();
+            x = sc.nextInt();
+            switch (x) {
+                case 0:
+                    System.out.println("Hasta luego");
+                    break;
+                case 1:
+                    if (sc.hasNextLine()) {sc.nextLine();} // Comprobar que esta limpio el scanner
+                    System.out.println("Escribe el texto:");
+                    faa.escribir(sc.nextLine());
+                    break;
+                case 2: // Coloca el cursor en la posición indicada.
+                    if (sc.hasNextLine()) {sc.nextLine();} // Comprobar que esta limpio el scanner
+                    System.out.println("¿A qué posición quieres ir?");
+                    faa.ir(sc.nextInt());
+                    break;
+                case 3: // Adelanta el cursor en las posiciones indicadas.
+                    if (sc.hasNextLine()) {sc.nextLine();} // Comprobar que esta limpio el scanner
+                    System.out.println("¿Cuántas posiciones quieres adelantar?");
+                    faa.adelante(sc.nextInt());
+                    break;
+                case 4: // Atrasa el cursor en las posiciones indicadas.
+                    if (sc.hasNextLine()) {sc.nextLine();} // Comprobar que esta limpio el scanner
+                    System.out.println("¿Cuántas posiciones quieres retrasar?");
+                    faa.atras(sc.nextInt());
+                    break;
+                case 5: // Muestra por pantalla el caracter apuntado por el cursor.
+                    faa.leer();
+                    break;
+                case 6: // Muestra el resto de la línea de la linea donde se encuentra el cursor.
+                    faa.leerLinea();
+                    break;
+                case 7: // Muestra el contenido de un fichero
+                    faa.mostrarFichero();
+                    break;
+            }
+        }
+    }
+    // Souts del menu
+    public static void menuPreguntas() {
+        System.out.println("0 para salir" + '\n'
+                + "1 para escribir el texto en el punto donde se encuentre el cursor del archivo." + '\n'
+                + "2 para coloca el cursor en la posición indicada." + '\n'
+                + "3 para adelantar el cursor en las posiciones indicadas." + '\n'
+                + "4 para atrasar el cursor en las posiciones indicadas." + '\n'
+                + "5 para mostrar por pantalla el caracter apuntado por el cursor." + '\n'
+                + "6 para mostrar el resto de la línea de la linea donde se encuentra el cursor." + '\n'
+                + "7 para mostrar el contenido de un fichero");
     }
 }
