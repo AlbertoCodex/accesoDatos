1d773450df00904deea723cdffdf114ac1f9b220
AlbertoCodex <zenycs3@gmail.com>
2024-10-08 17:11:59 +0200

    Crear proyecto + estructura

---
+++ Agenda/src/Agenda.java
@@ -0,0 +1,32 @@
+public class Agenda {
+    // Constructor vacio
+    public Agenda() {}
+    // Nueva entrada al mapa
+    public void insertar(String nombre, int numero) {
+
+    }
+    // Devuelve el número asociado al nombre
+    public int buscar(String nombre) {
+        return 0;
+    }
+    // Devuelve el número de contactos de la agenda.
+    public int longitud() {
+        return 0;
+    }
+    // Muestra toda la agenda.
+    public void ver() {
+
+    }
+    // Borra el contacto con ese nombre
+    public void borrar(String nombre) {
+
+    }
+    // Modifica el número de telefono (por el pasado como argumento) del nombre solicitado.
+    public void modificar(String nombre, int numero) {
+
+    }
+    // Metodo para convertir la información en un String[] (opcional).
+    public void exportar() {
+
+    }
+}
---
+++ Agenda/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class Main {
+    private static HashMap<String, Integer> personas;
+    public static void main(String[] args) {
+         personas = new HashMap<>();
+    }
+}
